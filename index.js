a5b36da8bcf913a77e994ff7edf9353a631e7270
shahid1105 <shahidhasan1105@gmail.com>
2023-07-23 19:49:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middle ware 
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('campus reserve is running')
+})
+
+app.listen(port, () => {
+    console.log(`campus reserve is running on port ${port}`);
+})
